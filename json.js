2937df0255de0d12d76ec5cb4b63b0c4a5ef3c8d
Sathishsooraa2312 <99782120+Sathishsooraa2312@users.noreply.github.com>
2022-08-19 08:42:53 +0530

    Add files via upload

---
+++ json.js
@@ -0,0 +1,21 @@
+var resume={
+    name:"sathish kumar",
+    age:24,
+    Dateofbirth:"23-12-1997",
+    degree:"bachelor of engineering" ,
+    coursename:"electronics and communication engineering",
+    email:"sathishrohin22@gmail.com",
+    contactnumber:8489980547,
+    coursesknown:"java,html,css,dom,react,nodejs",
+    address:{
+        houseno:295,
+        streetname:"rajeshwari street",
+        area:"munjurpattu",
+        district:"vellore"
+    }
+  
+}
+resume.coursecompletion=2019;
+
+
+console.log(JSON.stringify(resume));
